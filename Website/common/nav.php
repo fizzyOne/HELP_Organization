7249ff253066a7c95e7b11955b858f076a172fc7
fizzyOne <amdyhafiz@gmail.com>
2018-10-26 02:06:41 +0500

    html to php

---
+++ Website/common/nav.php
@@ -0,0 +1,13 @@
+<div class="container">
+	<ul class="nav nav-pills">
+	  <li class="nav-item">
+	    <a class="nav-link" href="index.php">Home</a>
+	  </li>
+	  <li class="nav-item">
+	    <a class="nav-link" href="about.php">About Us</a>
+	  </li>
+	  <li class="nav-item">
+	    <a class="nav-link" href="contact.php">Contact</a>
+	  </li>
+	</ul>
+</div>
