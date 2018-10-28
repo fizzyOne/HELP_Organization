83ab5296efa6aefdf7eb6445771558c6c9e17bf2
fizzyOne <amdyhafiz@gmail.com>
2018-10-28 15:41:06 +0500

    major fallback

---
+++ common/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <p style="float: right;">Help everyone, together we can</p>
+    <p><a href="https://www.facebook.com/HELP">facebook/Help_Organizaiton</a></p>
+
+    <p><a href="https://www.instagram.com/HELP">instagram/Help_Organizaiton</a></p>
+
+    <p><a href="https://t.me/HELP">telegram/Help_Organizaiton</a></p>
+
+    <p style="text-align: center;">&copy; HELP ORGANIZATION
+        <?php echo date("Y");?>
+    </p>
+
+
+</footer>
